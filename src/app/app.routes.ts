55656917470d4dce7d29a809fab5004d2b329ddb
ChrisKeo0MG <al349312@edu.uaa.mx>
2025-12-13 11:39:21 -0600

    cambios completos al main

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { QyAComponent } from './qy-a/qy-a.component';
+import { SobreNosostrosComponent } from './sobre-nosostros/sobre-nosostros.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'inicio', component: InicioComponent},
+    {path: 'qya', component: QyAComponent},
+    {path: 'nosotros', component: SobreNosostrosComponent},
+    {path: 'login', component: LoginComponent},
+    {path: '**', pathMatch: 'full', redirectTo: 'inicio' },
+];
---
+++ src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-nav-bar',
+  imports: [RouterModule],
+  standalone:true,
+  templateUrl: './nav-bar.component.html',
+  styleUrl: './nav-bar.component.css'
+})
+export class NavBarComponent {
+
+}
